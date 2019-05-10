9fead70497ae46cb80a1fec47f6677f4410f1154
m45adiwinata <m45adiwinata@gmail.com>
2019-05-11 00:52:35 +0800

    preprocessing

--- research.py
+++ research.py
@@ -6,6 +6,7 @@ Created on Fri Apr 12 01:09:02 2019
 """
 
 import os
+import numpy as np
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
 factory = StopWordRemoverFactory()
@@ -22,18 +23,20 @@ cerpens = []
 for p in path:
     cerpens.append([os.path.join(p,fname) for fname in os.listdir(p) if fname.endswith('.txt')])
 
-datas= []
+kelasdata = []
 for cerpen in cerpens:
+    datas = []
     for cer in cerpen:
         contents = open(cer, 'r')
         kalimat = []
         for c in contents:
             c = c.strip()
             kalimat.append(c)
-        data = []
+        data = np.array([])
         for k in kalimat:
             katastop = stopword.remove(k)
             katadasar = stemmer.stem(katastop)
             katastop = stopword.remove(katadasar)
-            data.append(katastop.split(' '))
+            data = np.append(data, katastop.split(' '))
         datas.append(data)
+    kelasdata.append(datas)
