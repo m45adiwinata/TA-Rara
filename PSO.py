46281e738de986a9c61230858a85b999b980887d
yogiprasetya <yogisetya17@gmail.com>
2019-05-28 18:34:09 +0800

    kfc

--- PSO.py
+++ PSO.py
@@ -17,14 +17,12 @@ for t in file:
     t = t.strip()
     terms = t.split(' ')
 file.close()
-
-bobot_awal = np.array(pd.read_excel('bobot awal.xlsx'))
 IDF = np.array(pd.read_excel('IDF.xlsx'))
 
 def naive_bayes(all_W, W_uji, term_used):
-    pr_A = 175/float(500)
-    pr_F = 175/float(500)
-    pr_TD = 150/float(500)
+    pr_A = 125/float(350)
+    pr_F = 125/float(350)
+    pr_TD = 100/float(350)
     p_term = []
     all_W = np.array(all_W)
     total_t = sum(all_W[0,:]) + sum(all_W[1,:]) + sum(all_W[2,:])
@@ -54,28 +52,28 @@ def hitung_fitness(alpha, beta, total_W_a, total_W_f, total_W_td, populasi):
         for j in range(125):
             P = naive_bayes(all_W, W[:,j], term_used[i])
             result.append(np.argmax(P))
-        for j in range(175, 300):
+        for j in range(125, 250):
             P = naive_bayes(all_W, W[:,j], term_used[i])
             result.append(np.argmax(P))
-        for j in range(350, 450):
+        for j in range(250, 350):
             P = naive_bayes(all_W, W[:,j], term_used[i])
             result.append(np.argmax(P))
         result = np.array(result)
         if np.argwhere(result == 0).size > 0:
-            precisions = [np.argwhere(result[:150] == 0).size/float(np.argwhere(result == 0).size)]
+            precisions = [np.argwhere(result[:125] == 0).size/float(np.argwhere(result == 0).size)]
         else:
             precisions = [0]
         if np.argwhere(result == 1).size > 0:
-            precisions.append(np.argwhere(result[150:300] == 1).size/float(np.argwhere(result == 1).size))
+            precisions.append(np.argwhere(result[125:250] == 1).size/float(np.argwhere(result == 1).size))
         else:
             precisions.append(0)
         if np.argwhere(result == 2).size > 0:
-            precisions.append(np.argwhere(result[300:] == 2).size/float(np.argwhere(result == 2).size))
+            precisions.append(np.argwhere(result[250:] == 2).size/float(np.argwhere(result == 2).size))
         else:
             precisions.append(0)
-        recalls = [np.argwhere(result[:150] == 0).size/float(150)]
-        recalls.append(np.argwhere(result[150:300] == 1).size/float(150))
-        recalls.append(np.argwhere(result[300:] == 2).size/float(100))
+        recalls = [np.argwhere(result[:125] == 0).size/float(125)]
+        recalls.append(np.argwhere(result[125:250] == 1).size/float(125))
+        recalls.append(np.argwhere(result[250:] == 2).size/float(100))
         Fmeasures = []
         for j in range(3):
             Fmeasures.append(2 * recalls[j] * precisions[j] / (recalls[j] + precisions[j]))
@@ -87,7 +85,7 @@ b_inersia = 0.6
 c1 = 0.5
 c2 = 0.5
 #INISIASI POPULASI
-populasi = np.zeros((30,len(terms)-1))
+populasi = np.zeros((30,len(terms)))
 for i in range(populasi.shape[0]):
     for j in range(populasi.shape[1]):
         populasi[i,j] = random.randint(0,1)
@@ -103,9 +101,9 @@ for i in range(populasi.shape[0]):
     for j in range(populasi.shape[1]):
         if populasi[i,j] == 1:
             temp.append(j)
-            tmp_a.append(sum(W[j,:175]))
-            tmp_f.append(sum(W[j,175:350]))
-            tmp_td.append(sum(W[j,350:]))
+            tmp_a.append(sum(W[j,:125]))
+            tmp_f.append(sum(W[j,125:250]))
+            tmp_td.append(sum(W[j,250:]))
     term_used.append(temp)
     total_W_a.append(tmp_a)
     total_W_f.append(tmp_f)
@@ -113,7 +111,7 @@ for i in range(populasi.shape[0]):
 #HITUNG FITNESS
 alpha = 0.85
 beta = 0.15
-v = np.ones((populasi.shape))
+v = np.zeros((populasi.shape))
 for i in range(2):
     fitness = hitung_fitness(alpha, beta, total_W_a, total_W_f, total_W_td, populasi)
     if i == 0:
@@ -129,7 +127,7 @@ for i in range(2):
     gbest_val = fitness[np.argmax(fitness)]
     for j in range(v.shape[0]):
         for k in range(v.shape[1]):
-            v[j,k] = b_inersia * v + c1 * random.randint(0,1) * (pbest_val[j] - populasi[j,k]) + c2 * random.randint(0,1) * (gbest_val - populasi[j,k])
+            v[j,k] = b_inersia * v + c1 * random.random() * (pbest_val[j] - populasi[j,k]) + c2 * random.random() * (gbest_val - populasi[j,k])
             sig_v = 1 / (1 + pow(math.e, (-v[j,k])))
             if populasi[j,k] < sig_v:
                 populasi[j,k] = 1
