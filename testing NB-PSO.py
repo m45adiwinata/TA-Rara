d6aa61e6703107dbdb528495b5b8f172c611b88b
m45adiwinata <m45adiwinata@gmail.com>
2019-06-13 21:35:53 +0800

    research perbaikan

--- testing NB-PSO.py
+++ testing NB-PSO.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 12 01:09:02 2019
+Created on Tue Jun 11 17:19:15 2019
 
 @author: Grenceng
 """
 
-import os
+import pandas as pd
 import numpy as np
+import random
 import math
+import time
+import os
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
-import pandas as pd
 
 factory = StopWordRemoverFactory()
 stopword = factory.create_stop_word_remover()
 factory = StemmerFactory()
 stemmer = factory.create_stemmer()
 
-data_path = open('data path.txt', 'r')
+data_path = open('testing data path.txt', 'r')
 path = []
 for p in data_path:
     p = p.strip()
@@ -41,7 +43,32 @@ def tokenize(kalimat):
                     term = []
         token.append(('').join(term))
     return (' ').join(token)
-    
+
+def naive_bayes(all_W, W_uji, term_used):
+    pr_A = 125/float(350)
+    pr_F = 125/float(350)
+    pr_TD = 100/float(350)
+    p_term = []
+    all_W = np.array(all_W)
+    total_t = sum(all_W[0,:]) + sum(all_W[1,:]) + sum(all_W[2,:])
+    for i in range(len(all_W)):
+        temp = []
+        for j in range(len(all_W[i])):
+            P = (all_W[i,j] + 1) / float(total_t + sum(all_W[i,:]))
+            temp.append(P)
+        p_term.append(temp)
+    P = []
+    for i in range(len(p_term)):
+        temp = 1
+        for j in range(len(p_term)):
+            if W_uji[term_used[j]] > 0:
+                temp *= p_term[i][j]
+        P.append(temp)
+    P[0] *= pr_A
+    P[1] *= pr_F
+    P[2] *= pr_TD
+    return P
+
 datas = []
 for cerpen in cerpens:
     for cer in cerpen:
@@ -61,10 +88,8 @@ for cerpen in cerpens:
                 temp = len(katastop)
                 katastop = stopword.remove(katastop)
             data = np.append(data, katastop.split(' '))
-            data = np.delete(data, np.argwhere(data == '').flatten())
         datas.append(data)
 
-#pembobotan
 terms = np.array([])
 for i in range(len(datas)):
     for j in range(datas[i].size):
@@ -73,31 +98,25 @@ for i in range(len(datas)):
         else:
             if np.argwhere(terms == datas[i][j]).size == 0:
                 terms = np.append(terms, datas[i][j])
+                
+learned_terms = np.array([])
+file = open('term gbest.txt', 'r')
+for f in file:
+    f = f.strip()
+    learned_terms = np.append(learned_terms, f.split(' '))
 
-tf = np.zeros((terms.size, 350))
-for i in range(len(datas)):
-    for j in range(len(datas[i])):
-        for k in range(tf.shape[0]):
-            if terms[k] == datas[i][j]:
-                tf[k][i] += 1
-
-IDF = np.array([])
-for i in range(terms.size):
-    D = len(datas)
-    df = len(np.nonzero(tf[i,:])[0])
-    IDF = np.append(IDF, math.log(D/df))
-
-W = np.zeros((len(datas), terms.size))
-for i in range(W.shape[0]):
-    for j in range(W.shape[1]):
-        W[i,j] = tf[j,i] * (IDF[j] + 1)
-df = pd.DataFrame(W)
-df.to_excel('bobot awal.xlsx', index=False)
-tf = pd.DataFrame(tf)
-tf.to_excel('term frequency.xlsx', index=False, header=False)
-file = open('terms.txt', 'w')
-for t in terms:
-    file.write("%s " % t)
-file.close()
-idf = pd.DataFrame(IDF)
-idf.to_excel('IDF.xlsx', index=False, header=False)
+bobot = np.array(pd.read_excel('Bobot Gbest.xlsx'))
+results = []
+for data in datas:
+    total_used_W_a = []
+    total_used_W_f = []
+    total_used_W_td = []
+    term_used = []
+    for i in range(learned_terms.size):
+        if np.argwhere(learned_terms[i] == data).size > 0:
+            total_used_W_a.append(np.sum(bobot[i,:125]))
+            total_used_W_f.append(np.sum(bobot[i,125:250]))
+            total_used_W_td.append(np.sum(bobot[i,250:]))
+            term_used.append(i)
+    all_W = [total_used_W_a, total_used_W_f, total_used_W_td]
+    P = naive_bayes(all_W, )
